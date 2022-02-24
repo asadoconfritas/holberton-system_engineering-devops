e6450921a34ec4ac1c5525c555655a1add360e90
root <guzchelsea24@gmail.com>
2022-02-23 17:50:40 -0800

    log n 100

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,5 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/[A-Z]/).join
+from = ARGV[0].scan(/(?<=from:)[^\[\]]+/).join
+to = ARGV[0].scan(/(?<=to:)[^\[\]]+/).join
+flags = ARGV[0].scan(/(?<=flags:)[^\[\]]+/).join
+puts "#{from},#{to},#{flags}"
