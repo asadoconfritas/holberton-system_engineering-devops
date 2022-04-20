2c7dc5aba7fbe41f6893bd03d5beca8ba8f64044
root <guzchelsea24@gmail.com>
2022-04-19 19:59:11 -0700

    updating files

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,8 @@
-#
-
-xd
+# setting up a client SSH configuration file
+file { '/etc/ssh/ssh_config':
+  ensure  => 'present',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0744',
+  content => 'Host * \n PasswordAuthentication no\n IdentityFile ~/.ssh/school'
+}
