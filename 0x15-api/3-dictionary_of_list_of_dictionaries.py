3e840f37dead61b8813b07a7a4cc1f2fc6cf8319
root <guzchelsea24@gmail.com>
2022-05-16 20:10:01 -0700

    upd 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -6,28 +6,21 @@ import requests
 from sys import argv
 
 
-def get_todos2(u_id):
-    """export to json format"""
+if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/'
-    evruser = json.loads(requests.get('{}users/{}'.format(url, u_id)).text)
-    data = requests.get('{}users/{}/todos'.format(url, u_id))
-    content = json.loads(data.text)
+    users = json.loads(requests.get('{}users/{}'.format(url, u_id)).text)
 
     keys = ['id', 'name', 'completed', 'title']
-    with open('{}.json'.format(u_id), 'wt') as file:
-        dictionary = []
-        aux = {}
-        aux[u_id] = []
-        for value in content:
-            task_dict = {}
-            task_dict['task'] = value['title']
-            task_dict['completed'] = value['completed']
-            task_dict['username'] = user['username']
-            aux[u_id].append(task_dict)
-        json.dump(aux, file)
-        dict_writer = csv.DictWriter(file,
-
-
-if __name__ == '__main__':
-    if len(argv) >= 2:
-        get_todos2(argv[1])
+    aux = {}
+    with open('todo_all_employees.json', 'wt') as file:
+        for u in users:
+            todo_list = requests.get('{}users/{}/todos'.format(url, u['id']))
+            todo_list = json.loads(todo_list.text)
+            aux[u['id']] = []
+            for value in todo_list:
+                task_dict = {}
+                task_dict['task'] = value['title']
+                task_dict['completed'] = value['completed']
+                task_dict['username'] = u['username']
+                aux[u['id']].append(task_dict)
+            json.dump(aux, file)
