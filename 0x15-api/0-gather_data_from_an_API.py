7bad79e9feb6a08497352d5b5c9ca43a975daaf4
root <guzchelsea24@gmail.com>
2022-05-16 19:59:21 -0700

    upd 0n1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,3 +1,25 @@
 #!/usr/bin/python3
-"""comment"""
-a
+"""returns information about an employee TODO list progress."""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    todos = 'https://jsonplaceholder.typicode.com/todos?userId={}'\
+            .format(int(argv[1]))
+    allusers = 'https://jsonplaceholder.typicode.com/users/{}'\
+            .format(int(argv[1]))
+    counter = 0
+    tasks = 0
+    ret = requests.get(todos).json()
+    user = requests.get(allusers).json()
+    for elem in ret:
+        if elem.get('completed') is True:
+            counter += 1
+        tasks += 1
+    print('Employee {} is done with tasks({}/{}):'
+            .format(user.get('name'), done, tasks))
+    for elem in ret:
+        if elem.get('completed') is True:
+            print('\t {}'.format(elem.get('title')))
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""returns information about an employee TODO list progress."""
+import json
+import csv
+import requests
+from sys import argv
+
+
+def get_todos(u_id):
+    """export to csv format"""
+    url = 'https://jsonplaceholder.typicode.com/'
+    evruser = json.loads(requests.get('{}users/{}'.format(url, u_id)).text)
+    data = requests.get('{}users/{}/todos'.format(url, u_id))
+    content = json.loads(data.text)
+
+    keys = ['id', 'name', 'completed', 'title']
+    with open('{}.csv'.format(u_id), 'wt') as file:
+        for value in content:
+            value['id'] = u_id
+            value['name'] = evruser['username']
+        dict_writer = csv.DictWriter(file,
+                keys,
+                extrasaction='ignore',
+                quoting=csv.QUOTE_ALL,
+                lineterminator='\n')
+        dict_writer.writerows(content)
+
+
+if __name__ == '__main__':
+    if len(argv) >= 2:
+        get_todos(argv[1])
